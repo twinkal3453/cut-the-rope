0827922bb23f64036c534619a680ac122c3d6b5a
twinkal3453 <twinkalkrraj@gmail.com>
2019-09-19 01:23:04 +0530

    Add files via upload

---
+++ cutscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutscript : MonoBehaviour
+{
+    public Camera cam;
+
+
+   //use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D ray = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(ray.collider != null)
+            {
+                if(ray.collider.tag == "Rope")
+                {
+                    Destroy(ray.collider.gameObject);
+                }
+            }
+        }
+    }
+}
